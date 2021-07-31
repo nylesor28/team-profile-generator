ea8fb97de0d9c375f114491d4ba96dfb57b2d2a2
RD <81473475+nylesor28@users.noreply.github.com>
2021-07-31 15:44:19 -0400

    hard code role for Employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,7 +3,7 @@ const  Employee = require('../lib/Employee');
 
 
 test("creates an employee object",()=>{
-    const employee = new Employee("Titan", 55, "noemail@gmail.com","Employee");
+    const employee = new Employee("Titan", 55, "noemail@gmail.com");
     expect(employee.name).toBe("Titan");
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toMatch(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/);
@@ -12,21 +12,21 @@ test("creates an employee object",()=>{
 
 
 test("gets employee's name",()=>{
-    const employee = new Employee("Titan", 55, "noemail@gmail.com","Employee");
+    const employee = new Employee("Titan", 55, "noemail@gmail.com");
     expect(employee.getName()).toEqual(expect.any(String));
 })
 
 test("gets employee's id",()=>{
-    const employee = new Employee("Titan", 55, "noemail@gmail.com","Employee");
+    const employee = new Employee("Titan", 55, "noemail@gmail.com");
     expect(employee.getID()).toEqual(expect.any(Number));
 })
 
 test("gets employee's email",()=>{
-    const employee = new Employee("Titan", 55, "noemail@gmail.com","Employee");
+    const employee = new Employee("Titan", 55, "noemail@gmail.com");
     expect(employee.getEmail()).toMatch(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/);
 })
 
 test("gets employee's role",()=>{
-    const employee = new Employee("Titan", 55, "noemail@gmail.com","Employee");
+    const employee = new Employee("Titan", 55, "noemail@gmail.com");
     expect(employee.getRole()).toEqual("Employee");
 })
